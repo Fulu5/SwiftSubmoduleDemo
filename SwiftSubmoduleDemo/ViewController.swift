61ff7d02ed1182ef1f15de5b6e2837a9338a1937
gxcao <1120643432@qq.com>
2016-12-26 14:37:20 +0800

    Add frameworks with submodule

--- SwiftSubmoduleDemo/ViewController.swift
+++ SwiftSubmoduleDemo/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("http://api.kufm.cn/v3/get_dj/location/0/order/1/page_info?bar_height=81&page_index=0&page_size=20&total_height=627").responseJSON { (response) in
+            print(response)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
